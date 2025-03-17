b31552c70ef47391cc8a83da5b1b586ed318636d
Konstantin Kutsyllo <trikadin@pm.me>
2025-03-17 21:20:47 +0400

    update tsconfigs package structure

---
+++ packages/lib-es/src/object.ts
@@ -0,0 +1,126 @@
+import type { Key, OmitNilProps, Value } from '@budsbox/types/object';
+
+import type { ConditionalExcept } from 'type-fest';
+
+import { isNotNil } from './guards.js';
+
+export function pick<T extends object, Keys extends PropertyKey = keyof T>(
+  source: T,
+  ...keys: readonly Keys[]
+): Pick<T, Keys & keyof T>;
+export function pick(
+  source: Record<PropertyKey, unknown>,
+  ...keys: readonly PropertyKey[]
+): Record<PropertyKey, unknown> {
+  return keys.reduce(
+    (acc, key) =>
+      Object.hasOwn(source, key) ? { ...acc, [key]: source[key] } : acc,
+    {},
+  );
+}
+
+export const pickStrict: <T extends object, Keys extends keyof T = keyof T>(
+  source: T,
+  ...keys: readonly Keys[]
+) => Pick<T, Keys> = pick;
+
+export function omit<T extends object, K extends PropertyKey>(
+  source: T,
+  ...keys: readonly K[]
+): Omit<T, K>;
+export function omit(source: object, ...keys: readonly PropertyKey[]): object {
+  const keySet = new Set(keys);
+
+  return Object.fromEntries(
+    Object.entries(source).filter(([key]) => !keySet.has(key)),
+  );
+}
+
+export const omitStrict: <T extends object, Key extends keyof T>(
+  source: T,
+  ...keys: readonly Key[]
+) => Omit<T, Key> = omit;
+
+export function filterBy<T, K extends string, R extends T>(
+  value: Partial<Record<K, T>>,
+  test: (value: T) => value is R,
+): ConditionalExcept<Record<K, T>, R>;
+export function filterBy<T, K extends string, R extends T>(
+  // eslint-disable-next-line @typescript-eslint/unified-signatures
+  value: Partial<Record<K, T>> | Record<K, T>,
+  test: (value: T) => value is R,
+): ConditionalExcept<Record<K, T>, R>;
+export function filterBy<V extends object>(
+  obj: V,
+  filter: (value: V[keyof V], key: keyof V, obj: V) => boolean,
+): Partial<V>;
+export function filterBy(
+  obj: object,
+  filter: (value: unknown, key: PropertyKey, obj: object) => boolean,
+): object {
+  const newObj: Record<string, unknown> = {};
+
+  for (const key in obj) {
+    if (!Object.hasOwn(obj, key)) {
+      break;
+    }
+
+    const value = (obj as Record<string, unknown>)[key];
+
+    if (filter(value, key, obj)) {
+      newObj[key] = value;
+    }
+  }
+
+  return newObj;
+}
+
+export function omitNils<T extends object>(object: T): OmitNilProps<T> {
+  return filterBy(object, isNotNil) as never;
+}
+
+export function reduce<U>(
+  obj: Readonly<Record<string, unknown>>,
+  callback: (
+    previousValue: U,
+    currentValue: Value<typeof obj>,
+    currentKey: keyof typeof obj,
+    object: typeof obj,
+  ) => U,
+  initialValue: Partial<U>,
+): U {
+  return Object.entries(obj).reduce<U>(
+    (acc, [key, value]) => callback(acc, value, key, obj),
+    initialValue,
+  );
+}
+
+/**
+ * Returns the first key of the given object.
+ * Useful when working with objects that have only one key.
+ *
+ * @param value
+ */
+
+export function getKey<T extends string>(value: Record<T, unknown>): T;
+export function getKey<T extends string>(
+  value: Partial<Record<T, unknown>>,
+): T | undefined;
+export function getKey<T extends object>(value: T): keyof T;
+export function getKey(value: object): Key | undefined {
+  for (const key in value) {
+    if (Object.hasOwn(value, key)) {
+      return key;
+    } else {
+      /*
+       Modern JS interpreters in the for..in loop
+        first traverse the object's own properties,
+        and then the inherited ones. Therefore, you can end the loop
+        if the own property has never been encountered
+       */
+      break;
+    }
+  }
+
+  return undefined;
+}
---
+++ packages/lib-es/src/string.ts
@@ -0,0 +1,139 @@
+import type {
+  CamelCase,
+  DelimiterCase,
+  KebabCase,
+  PascalCase,
+  SnakeCase,
+} from 'type-fest';
+
+import { isNotNil } from './guards.js';
+
+/**
+ * Parses the given NPM package name to extract its namespace and name.
+ *
+ * @param packageName The full package name as a string, which may include an optional namespace (e.g., `@namespace/name`).
+ * @returns An object containing:
+ *         - `ns`: The namespace of the package prefixed with `@` and suffixed with `/`, or `null` if no namespace is present.
+ *         - `name`: The name of the package without the namespace.
+ */
+export function parsePackageName(packageName: string): {
+  ns: `@${string}/` | null;
+  name: string;
+} {
+  const [ns] = (/^@[^/]+\//.exec(packageName) as [`@${string}/`] | null) ?? [
+    null,
+  ];
+  return {
+    ns,
+    name: packageName.replace(ns ?? '', ''),
+  };
+}
+
+/**
+ * Returns the string representation of the given value.
+ * Useful for debugging purposes.
+ *
+ * @param value
+ */
+export function debugString(value: unknown): string {
+  try {
+    return JSON.stringify(value);
+  } catch {
+    return String(value);
+  }
+}
+
+/**
+ * Trims leading and trailing whitespace from the input string and collapses
+ * multiple consecutive whitespace characters into a single space.
+ *
+ * @param str - The input string to be processed.
+ * @returns The processed string with trimmed and normalized whitespace.
+ */
+export function clampWS(str: string): string {
+  return str.trim().replace(/\s+/g, ' ');
+}
+
+/**
+ * Joins multiple parts of a path into a single string. It ensures that there are no duplicate slashes between the parts and trims leading/trailing slashes where necessary.
+ *
+ * @param parts - An array of path parts to join. Each part can be a string, number, boolean, null, or undefined. Non-string values will be stringified, and null/undefined values are ignored.
+ * @returns The combined path as a single string, with proper slash formatting.
+ */
+export function joinPath(
+  ...parts: ReadonlyArray<string | number | boolean | null | undefined>
+): string {
+  return parts
+    .filter(isNotNil)
+    .reduce<string>(
+      (acc, part) =>
+        acc.length === 0 ?
+          String(part)
+        : `${acc.replace(/\/+$/, '')}/${String(part).replace(/^\/+/, '')}`,
+      '',
+    );
+}
+
+/**
+ * Transforms a given string into camelCase format.
+ *
+ * @param str - The string to be converted to camelCase.
+ * @returns The input string transformed into camelCase.
+ */
+export function camelCase<T extends string>(str: T): CamelCase<T>;
+export function camelCase(name: string): string {
+  return clampWS(name).replace(/[-_\s]+([^-_\s])/g, (_, suffix: string) =>
+    suffix.toUpperCase(),
+  );
+}
+
+/**
+ * Converts a given string to PascalCase format.
+ *
+ * @param str - The string to be transformed into PascalCase.
+ * @returns The transformed string in PascalCase format.
+ */
+export function pascalCase<T extends string>(str: T): PascalCase<T>;
+export function pascalCase(name: string): string {
+  return camelCase(name).replace(/^./, (c) => c.toUpperCase());
+}
+
+/**
+ * Converts a given string to a delimited case using the specified delimiter.
+ *
+ * @param name - The input string that will be converted to the delimited case.
+ * @param delimiter - The character or string to use as a delimiter in the transformed output.
+ * @returns The input string transformed into the delimited case format using the given delimiter.
+ */
+export function delimCase<T extends string, D extends string>(
+  name: T,
+  delimiter: D,
+): DelimiterCase<T, D>;
+export function delimCase(name: string, delimiter = '-'): string {
+  return clampWS(name)
+    .replace(/(.)([A-Z])/g, `$1${delimiter}$2`)
+    .replace(/[-_\s]+/g, delimiter)
+    .toLowerCase();
+}
+
+/**
+ * Converts a given string to kebab-case format.
+ *
+ * @param name - The string input to be converted to kebab-case. The input should be a string type.
+ * @returns The transformed string in kebab-case format.
+ */
+export function kebabCase<T extends string>(name: T): KebabCase<T>;
+export function kebabCase(name: string): string {
+  return delimCase(name, '-');
+}
+
+/**
+ * Converts a given string to snake_case format.
+ *
+ * @param name - The string to be converted to snake_case.
+ * @returns The converted string in snake_case format.
+ */
+export function snakeCase<T extends string>(name: T): SnakeCase<T>;
+export function snakeCase(name: string): string {
+  return delimCase(name, '_');
+}
