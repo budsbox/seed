317773799ef905cb6e27fb7a0912f424e318106d
Konstantin Kutsyllo <trikadin@pm.me>
2025-04-30 22:32:20 +0400

    commit `@budsbox/build` "as is"

---
+++ build/src/bin/build-lib.ts
@@ -0,0 +1,37 @@
+import { cwd, env } from 'node:process';
+
+import meow from 'meow';
+
+import { build, TargetModule } from '#lib-builder';
+
+const cli = meow({
+  importMeta: import.meta,
+  description: 'TS libraries builder',
+  allowUnknownFlags: false,
+  flags: {
+    watch: {
+      type: 'boolean',
+      default: false,
+    },
+    module: {
+      type: 'string',
+      choices: ['esm', 'commonjs', 'dual'],
+      default: 'dual',
+    },
+    tsconfigFile: {
+      type: 'string',
+      isRequired: true,
+      default: 'tsconfig.lib.json',
+    },
+    force: {
+      type: 'boolean',
+      default: false,
+    },
+  },
+});
+
+build({
+  tsconfigFile: cli.flags.tsconfigFile,
+  initDir: env.INIT_CWD ?? cwd(),
+  targetModule: cli.flags.module as TargetModule,
+}).then(() => console.log('success'));
