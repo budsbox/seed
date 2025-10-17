73c370ade07dcfbefc62e96595cce1f130947dc6
Konstantin Kutsyllo <trikadin@pm.me>
2025-10-17 17:04:44 +0400

    **fix(ui-components-media): simplify Vite config with `{ lib: true }`**

--- ui/components/media/vite.config.ts
+++ ui/components/media/vite.config.ts
@@ -1,26 +1,3 @@
-import packageJson from '#package.json' with { type: 'json' };
-import {
-  formatFileName,
-  formatVarName,
-  useReactConfig,
-} from '@budsbox/builder_vite';
+import { useReactConfig } from '@budsbox/builder_vite';
 
-export default useReactConfig(() => ({
-  build: {
-    lib: {
-      entry: 'index.ts',
-      name: formatVarName(packageJson.name),
-      fileName: formatFileName,
-      cssFileName: 'index',
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          'react': 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
-}));
+export default useReactConfig(() => ({}), { lib: true });
