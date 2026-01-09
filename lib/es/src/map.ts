7734e1625583622af38da0ab68ca81c5e4f65d3a
Konstantin Kutsyllo <trikadin@pm.me>
2026-01-10 02:37:57 +0400

    **feat(lib-es): add `set` and `map` utilities with read-only implementations**
    - Introduce `ROSet` and `ROMap` for immutable `Set` and `Map` functionalities.
    - Add `union` and `intersection` utility functions for `Set` operations.
    - Update `package.json` exports with new `set` and `map` modules.

---
+++ lib/es/src/map.ts
@@ -0,0 +1,117 @@
+import type { Nil } from '@budsbox/lib-types';
+
+/**
+ * A read-only Map implementation that conforms to the `ReadonlyMap` interface.
+ * This class provides a wrapper around `Map` to enforce immutability.
+ *
+ * @typeParam K - The type of keys maintained by this map.
+ * @typeParam V - The type of mapped values.
+ */
+export class ROMap<K, V> implements ReadonlyMap<K, V> {
+  /**
+   * Constructs a new instance of the class with an optional collection of entries.
+   *
+   * @param entries - An optional iterable object containing key-value pairs (tuples) to initialize the map.
+   * If not provided or set to `Nil`, the map will be empty.
+   * @typeParam K - The type of the keys in the map.
+   * @typeParam V - The type of the values in the map.
+   */
+  public constructor(entries?: Iterable<readonly [K, V]> | Nil) {
+    this.#map = new Map(entries);
+  }
+
+  /* eslint-disable jsdoc/require-returns */
+
+  /**
+   * Returns the number of key-value pairs in the map.
+   */
+  public get size(): number {
+    return this.#map.size;
+  }
+
+  /**
+   * Checks if the given key exists in the map.
+   * This method works as type guard and narrows the type of the key to check to the type of keys of the map.
+   *
+   * @param key - The key to check for existence in the map.
+   */
+  public has(key: unknown): key is K extends Nil ? K : K & {} {
+    return this.#map.has(key as never);
+  }
+
+  /**
+   * Returns the value associated with the given key, or `undefined` if the key does not exist in the map.
+   *
+   * @param key - The key of the element to return from the map.
+   */
+  public get(key: K): V | undefined {
+    return this.#map.get(key);
+  }
+
+  /**
+   * Executes the provided callback function once for each key-value pair in the map.
+   *
+   * @param callback - A function to execute for each key-value pair in the map.
+   * The callback receives the current value, key, and the map itself as arguments.
+   */
+  public forEach(callback: (value: V, key: K, map: ROMap<K, V>) => void): void;
+
+  /**
+   * Executes a provided callback function once for each key-value pair in the map.
+   *
+   * @param callback - A function that is called for each element in the map.
+   * It receives the current element's value, key, and the map itself as arguments.
+   * @param thisArg - A value to use as `this` when executing the callback function.
+   * @returns Returns nothing.
+   * @typeParam TThis - The type to use as `this` when executing the callback function.
+   */
+  public forEach<TThis>(
+    callback: (this: TThis, value: V, key: K, map: ROMap<K, V>) => unknown,
+    thisArg: TThis,
+  ): void;
+
+  // eslint-disable-next-line jsdoc/require-jsdoc
+  public forEach(
+    callback: (value: V, key: K, map: ROMap<K, V>) => unknown,
+    thisArg: unknown = globalThis,
+  ): void {
+    this.#map.forEach((value, key) => callback.call(thisArg, value, key, this));
+  }
+
+  /**
+   * Returns an iterable of entries in the map.
+   */
+  public [Symbol.iterator](): MapIterator<[K, V]> {
+    return this.#map[Symbol.iterator]();
+  }
+
+  /**
+   * Returns an iterable of key, value pairs for every entry in the map.
+   */
+  public entries(): MapIterator<[K, V]> {
+    return this.#map.entries();
+  }
+
+  /**
+   * Returns an iterable of keys in the map
+   */
+  public keys(): MapIterator<K> {
+    return this.#map.keys();
+  }
+
+  /**
+   * Returns an iterable of values in the map
+   */
+  public values(): MapIterator<V> {
+    return this.#map.values();
+  }
+
+  readonly #map: Map<K, V>;
+
+  /* eslint-enable jsdoc/require-returns */
+}
+
+// it seems a bit hacky, but I plan to use it as an actual implementation for the `ReadonlyMap` type, so it seems reasonable
+Object.defineProperty(ROMap.prototype, Symbol.toStringTag, {
+  value: 'ReadonlyMap',
+});
---
+++ lib/es/src/set.ts
@@ -0,0 +1,200 @@
+import type { IterableElement } from 'type-fest';
+
+import type {
+  AnyReadableSet,
+  AnySet,
+  ArrayItemsIntersection,
+  Nil,
+} from '@budsbox/lib-types';
+
+/**
+ * Computes the union of multiple sets and returns a new set containing all unique elements.
+ *
+ * @param sets - A collection of sets to be unified.
+ * @returns A new set containing all unique elements present in any of the input sets.
+ * @typeParam TSets - An array of sets whose elements extend the type `UnknownSet`.
+ */
+export const union = <TSets extends readonly AnyReadableSet[]>(
+  ...sets: TSets
+): Set<IterableElement<IterableElement<TSets>>> => {
+  const unionSet = new Set<IterableElement<IterableElement<TSets>>>();
+
+  for (const set of sets) {
+    for (const element of set) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      unionSet.add(element);
+    }
+  }
+
+  return unionSet;
+};
+
+/**
+ * Computes the intersection of multiple sets, returning a new set containing only the elements
+ * that are present in all the provided sets.
+ *
+ * @param sets - A variable number of sets to compute the intersection from.
+ * @returns A new set containing the elements that are common to all provided sets.
+ * @typeParam TSets - A tuple of sets, where each set contains elements of any type.
+ * The resulting set contains elements that are a union of all possible intersections
+ * of the provided sets' elements.
+ */
+export const intersection = <TSets extends readonly AnyReadableSet[]>(
+  ...sets: TSets
+): Set<ArrayItemsIntersection<ItemsOfSetsArray<TSets>>> => {
+  const { length } = sets;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const counters = new Map<any, number>();
+
+  for (const set of sets) {
+    for (const element of set) {
+      const count = counters.get(element) ?? 0;
+
+      counters.set(element, count + 1);
+    }
+  }
+
+  const intersectionSet = new Set<
+    ArrayItemsIntersection<ItemsOfSetsArray<TSets>>
+  >();
+  for (const [element, count] of counters) {
+    if (count === length) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      intersectionSet.add(element);
+    }
+  }
+
+  return intersectionSet;
+};
+
+/**
+ * A read-only implementation of the `Set` interface. This class allows for creating immutable sets,
+ * providing the same methods as a standard `Set`, but without mutation capabilities.
+ *
+ * @typeParam T - The type of elements stored in the set.
+ */
+export class ROSet<T = undefined> implements ReadonlySet<T> {
+  /**
+   * Constructs a new instance of the class.
+   *
+   * @param input - An iterable object that contains elements of type `T` to initialize the set,
+   * or `Nil` to create an empty set.
+   * @typeParam T - The type of the elements in the set.
+   */
+  public constructor(input?: Iterable<T> | Nil) {
+    this.#set = new Set(input);
+  }
+
+  /* eslint-disable jsdoc/require-returns */
+
+  /**
+   * Iterates over values in the set.
+   *
+   * @returns An iterator that yields the values of the set.
+   */
+  public [Symbol.iterator](): SetIterator<T> {
+    return this.#set[Symbol.iterator]();
+  }
+
+  /**
+   * Returns the number of elements in the set.
+   */
+  public get size(): number {
+    return this.#set.size;
+  }
+
+  /**
+   * Checks if the given item is present in the set.
+   * This method works as type guard and narrow the type of the type of the items of the set.
+   *
+   * @param item - The item to check for existence in the set.
+   * @returns Returns `true` if the item exists in the set; otherwise, returns `false`.
+   * @typeParam TValue - The type of the item being checked.
+   */
+  public has(item: unknown): item is T extends Nil ? T : T & {} {
+    return this.#set.has(item as never);
+  }
+
+  /**
+   * Iterates over each value of the set, invoking the provided callback function for each element.
+   *
+   * @param callback - A function that is executed for each element of the set.
+   * It receives the current value, the second occurrence of the same value, the set itself, and an optional `thisArg` context.
+   * @param thisArg - The value to use as `this` inside the callback function.
+   * @typeParam T - The type of elements stored in the set.
+   * @typeParam TThis - The type of the `this` context to be used inside the callback.
+   */
+  public forEach<TThis>(
+    callback: (
+      this: TThis,
+      value: T,
+      value2: T,
+      set: ROSet<T>,
+      thisArg?: unknown,
+    ) => void,
+    thisArg: TThis,
+  ): void;
+
+  /**
+   * Executes the provided callback function once for each value in the set, in insertion order.
+   *
+   * @param callback - A function to execute for each value in the set.
+   * The function is invoked with three arguments: the current value, the same value (to match the Map-like callback signature), and the set itself.
+   * @typeParam T - The type of elements in the set.
+   */
+  public forEach(callback: (value: T, value2: T, set: ROSet<T>) => void): void;
+
+  // eslint-disable-next-line jsdoc/require-jsdoc
+  public forEach(
+    callback: (value: T, value2: T, set: ROSet<T>) => void,
+    thisArg: unknown = globalThis,
+  ): void {
+    this.#set.forEach(
+      (value) => void callback.call(thisArg, value, value, this),
+    );
+  }
+
+  /**
+   * Returns an iterable of [v,v] pairs for every value `v` in the set.
+   */
+  public entries(): SetIterator<[T, T]> {
+    return this.#set.entries();
+  }
+
+  /**
+   * Despite its name, returns an iterable of the values in the set.
+   *
+   * @returns An iterable of the values in the set.
+   */
+  public keys(): SetIterator<T> {
+    return this.#set.keys();
+  }
+
+  /**
+   * Returns an iterable of values in the set.
+   *
+   * @returns An iterable of values in the set.
+   */
+  public values(): SetIterator<T> {
+    return this.#set.values();
+  }
+
+  /* eslint-enable jsdoc/require-returns */
+
+  readonly #set: Set<T>;
+}
+
+// it seems a bit hacky, but I plan to use it as an actual implementation for the `ReadonlySet`, so it seems reasonable
+Object.defineProperty(ROSet.prototype, Symbol.toStringTag, {
+  value: 'ReadonlySet',
+});
+
+type ItemsOfSetsArray<TSets extends readonly AnyReadableSet[]> =
+  TSets extends readonly [infer TSet, ...infer TRest] ?
+    [
+      TSet extends Set<infer T> ? T : never,
+      ...(TRest extends readonly AnySet[] ? ItemsOfSetsArray<TRest> : []),
+    ]
+  : TSets extends readonly [] ? []
+  : TSets extends ReadonlyArray<Set<infer T>> ? T[]
+  : never;
