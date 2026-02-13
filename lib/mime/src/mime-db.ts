8f9e5aacd658646d4dcff40ca94e4ad31c330eb5
Konstantin Kutsyllo <trikadin@pm.me>
2026-02-13 16:14:31 +0400

    **refactor(lib-mime): update internal types and utilities in `mime-db` module for improved clarity and type safety**
    - Replace `DistributedValueOf` with enhanced `DistributedPropValue` from `@budsbox/lib-types`.
    - Simplify `UndefinedToOptional`.

--- lib/mime/src/mime-db.ts
+++ lib/mime/src/mime-db.ts
@@ -4,7 +4,14 @@
  * @module
  */
 
-import type { KeysOfUnion, SetOptional } from 'type-fest';
+import type { KeysOfUnion } from 'type-fest';
+
+import type {
+  AnyRecord,
+  Def,
+  DistributedPropValue,
+  Infer,
+} from '@budsbox/lib-types';
 
 import mimeDbRaw from 'mime-db/db.json' with { type: 'json' };
 
@@ -12,10 +19,14 @@ export type MimeDbKey = keyof typeof mimeDbRaw;
 
 export type MimeDbSource = 'apache' | 'iana' | 'nginx';
 
-export type MimeDbRecord = Readonly<
-  UndefinedToOptional<{
-    [K in MimeDbRecordKeys]: Readonly<DistributedValueOf<MimeDbRecordUnion, K>>;
-  }> & { source?: MimeDbSource }
+export type MimeDbRecord = Infer<
+  Readonly<
+    UndefinedToOptional<{
+      [K in KeysOfUnion<MimeDbRecordUnion>]: Readonly<
+        DistributedPropValue<MimeDbRecordUnion, K>
+      >;
+    }> & { readonly source?: MimeDbSource }
+  >
 >;
 
 export type MimeDb = Record<`${string}/${string}` | MimeDbKey, MimeDbRecord>;
@@ -25,25 +36,14 @@ export const mimeDb = mimeDbRaw as MimeDb;
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ UTILITY TYPES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 type MimeDbRecordUnion = (typeof mimeDbRaw)[MimeDbKey];
-type MimeDbRecordKeys = KeysOfUnion<MimeDbRecordUnion>;
 
-type DistributedValueOf<TSource, TKey extends PropertyKey> =
-  TSource extends Record<TKey, infer TValue> ? TValue : undefined;
-
-type UndefinedToOptional<TSource extends Record<PropertyKey, unknown>> =
-  ExcludeFromValues<
-    SetOptional<
-      TSource,
-      keyof {
-        [K in keyof TSource as undefined extends TSource[K] ? K : never]: null;
-      }
-    >,
-    undefined
-  >;
-
-type ExcludeFromValues<
-  TSource extends Record<PropertyKey, unknown>,
-  TExclude,
-> = {
-  [K in keyof TSource]: Exclude<TSource[K], TExclude>;
-};
+type UndefinedToOptional<TSource extends AnyRecord> = Infer<
+  {
+    [K in keyof TSource as undefined extends TSource[K] ? never
+    : K]: TSource[K];
+  } & {
+    [K in keyof TSource as undefined extends TSource[K] ? K : never]?: Def<
+      TSource[K]
+    >;
+  }
+>;
